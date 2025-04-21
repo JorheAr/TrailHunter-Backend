08313ece8472d151a16e19b63beba8c6ccc66bdd
Jorge Arias <jariasmartin@safareyes.es>
2025-04-21 12:04:34 +0200

    ✨ Change jwt token duration

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -6,6 +8,8 @@ from config import Config
 from models import db
 from routes.auth_routes import auth_bp
 from flask_cors import CORS
+from routes.user_routes import user_bp
+
 
 
 app = Flask(__name__)
@@ -14,10 +18,16 @@ app.config.from_object(Config)
 CORS(app)
 db.init_app(app)
 migrate = Migrate(app, db)
+
+# Configuración de JWT
+app.config['JWT_SECRET_KEY'] = 'your-secret-key'
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=30)
+
 jwt = JWTManager(app)
 
 # Registrar rutas
 app.register_blueprint(auth_bp, url_prefix="/auth")
+app.register_blueprint(user_bp, url_prefix="/usuarios")
 
 if __name__ == "__main__":
     app.run(debug=True)
