8f8fdd4dd93e1c0ea72f352b95fc2be0631f349f
Jorge Arias <jariasmartin@safareyes.es>
2025-04-25 10:44:31 +0200

    🔒️ Change the email and password to .env

--- services/email_service.py
+++ services/email_service.py
@@ -1,14 +1,21 @@
 import smtplib
 from email.mime.text import MIMEText
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 
 def send_verification_email(email, token):
     verification_url = f"http://localhost:4200/verificar-correo?token={token}"
     msg = MIMEText(f"Por favor, verifica tu cuenta haciendo clic en este enlace: {verification_url}")
     msg["Subject"] = "Verifica tu cuenta"
-    msg["From"] = "trailhuntersupp@gmail.com"
+    msg["From"] = EMAIL_ADDRESS
     msg["To"] = email
 
     with smtplib.SMTP("smtp.gmail.com", 587) as server:
         server.starttls()
-        server.login("trailhuntersupp@gmail.com", "blof jbix onwa wzwd")
+        server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         server.send_message(msg)
